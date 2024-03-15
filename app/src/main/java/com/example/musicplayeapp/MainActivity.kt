aaacb79f0350ae00aa5f9b934089688b45b4bb9a
Bharathi Tummala <bharathi.tummala87@gmail.com>
2024-03-15 14:27:39 -0700

    added views

--- app/src/main/java/com/example/musicplayeapp/MainActivity.kt
+++ app/src/main/java/com/example/musicplayeapp/MainActivity.kt
@@ -15,32 +15,7 @@ import com.example.musicplayeapp.ui.theme.MusicPlayeAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MusicPlayeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_maim)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicPlayeAppTheme {
-        Greeting("Android")
     }
 }
